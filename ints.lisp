b96255a7f248f110e4a64890ab2f2e3fef8b599e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-29 21:51:33 -0400

    add some general settings and helper functions

--- ints.lisp
+++ ints.lisp
@@ -46,3 +46,22 @@
   (position n (nodes G) :test #'equal))
 
 (describe 'graph)
+
+(defvar num 100)
+(defvar iterations 20)
+(defparameter mg (make-instance 'graph))
+
+(loop for i from 0 to num do (
+  add-node mg (make-instance 'node :data i)))
+
+; Via https://bese.common-lisp.dev/docs/arnesi/html/api/function_005FIT.BESE.ARNESI_003A_003ANOOP.html
+(defun noop (&rest args)
+  "Do nothing."
+  (declare (ignore args))
+  (values))
+
+
+(defun prime (n)
+	(loop for x from 2 to (isqrt n)
+		never (zerop (rem n x))
+		finally (return T)))
